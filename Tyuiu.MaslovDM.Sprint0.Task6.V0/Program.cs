97744a76e3b513bab915196cfb5287e098731678
Allah <Allah@DESKTOP-I4366T2>
2026-09-17 03:46:38 +0500

    Task6

--- Tyuiu.MaslovDM.Sprint0.Task6.V0/Program.cs
+++ Tyuiu.MaslovDM.Sprint0.Task6.V0/Program.cs
@@ -7,6 +7,7 @@ namespace Tyuiu.MaslovDM.Sprint0.Task6.V0
         {
             int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
 
+
             Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(numsArray));
 
             Console.WriteLine("Разность элементов массива = " + DataService.SubtractionArray(numsArray));
