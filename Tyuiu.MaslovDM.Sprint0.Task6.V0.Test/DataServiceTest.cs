a8d7692661e1d994520fd8057ddf5ed4f128b996
Allah <Allah@DESKTOP-I4366T2>
2026-09-17 00:56:13 +0500

    Task5

---
+++ Tyuiu.MaslovDM.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.MaslovDM.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.MaslovDM.Sprint0.Task5.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionArrayValid()
+        {
+var numbers = new int[] { 1, 2, 3, 4, 5 };
+        }
+    }
+}
